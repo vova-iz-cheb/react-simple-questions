9b27ec616b80c33d739dd1f7fd76e6b8b32f0450
vova-iz-cheb <vovan1cheb@gmail.com>
2019-09-21 23:19:39 +0300

    Fixed path error in folder 'build' for local use

---
+++ build/precache-manifest.58c8714452adbb942607db5c720472ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc1105f8c99078870a19728023b2348",
+    "url": "./index.html"
+  },
+  {
+    "revision": "96713888933942ee5b89",
+    "url": "./static/css/main.00564eb0.chunk.css"
+  },
+  {
+    "revision": "6c4ad70e7a273ba5005c",
+    "url": "./static/js/2.0841aee6.chunk.js"
+  },
+  {
+    "revision": "96713888933942ee5b89",
+    "url": "./static/js/main.84ea89db.chunk.js"
+  },
+  {
+    "revision": "656ee55d84f95da3e3b7",
+    "url": "./static/js/runtime~main.296f0042.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d39da27803971345378289c18a3a64a.js"
+  "./precache-manifest.58c8714452adbb942607db5c720472ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
